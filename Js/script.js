df4a5581e14c5d69def1a08bf2957f15d58a5cdd
Jabes Voltatoni <voltatoni.si@gmail.com>
2024-04-04 17:41:00 -0300

    Add JS game

--- Js/script.js
+++ Js/script.js
@@ -1,37 +1,73 @@
 const FRONT = "card_front"
 const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
 
-let techs = ['bootstrap',
-    'css',
-    'electron',
-    'firebase',
-    'html',
-    'javascript',
-    'jquery',
-    'mongo',
-    'node',
-    'react'];
+  
+startGame();
 
+function startGame() {
+    initializeCards(game.createCardsFromTechs());
+};
 
-function createCardsFromTechs(techs) {
-    let cards = [];
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+    gameBoard.innerHTML = '';
+    game.cards.forEach(card => {
 
-    for(let tech of techs){
-        cards.push(createPairFromTech(tech));
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    })
+
+};
+
+function createCardContent(card, cardElement){
+
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+
+};
+
+function createCardFace(face, card, element){
+    let cardElementeFace = document.createElement('div');
+    cardElementeFace.classList.add(face);
+    if(face === FRONT){
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src= "./assets/images/" + card.icon + ".png";
+        cardElementeFace.appendChild(iconElement);
+    }else{
+        cardElementeFace.innerHTML = "&lt/&gt";
     }
-}
+    element.appendChild(cardElementeFace);
+};
 
-function createPairFromTech(tech){
-    
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
 
-    },{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }]
-}
+
+
+function flipCard() {
+
+    if (game.setCard(this.id))
+
+        this.classList.add("flip");
+       if(game.checkMatch()){
+            game.clearCards();
+       }else{
+        setTimeout(()=>{
+        let firstCardView = document.getElementById(game.firstCard.id);
+        let secondCardView = document.getElementById(game.secondCard.id);
+
+        firstCardView.classList.remove("flip");
+        secondCardView.classList.remove("flip");
+        game.clearCards();
+        }, 1000);
+       };
+};   
 
